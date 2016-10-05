dbf05f76c8968cf01d0e593730c9b5620084a95c
Michael Reuter <mareuter@lsst.org>
2016-10-05 08:44:46 -0700

    Polar viewer script cleanup.

--- live_view/opsim_live_polar.py
+++ live_view/opsim_live_polar.py
@@ -21,8 +21,8 @@ FILTER_DICT = collections.OrderedDict([('u', [0, 0, 1, 1]), ('g', [0, 1, 1, 1]),
 PI_OVER_2 = np.pi / 2.0
 
 def axisSetup(ax):
+    ax.set_rgrids([0.3333, 0.66666, 1.0], [u"60\u00b0", u"30\u00b0", u"0\u00b0"])
     ax.grid(True)
-    #ax.xaxis.set_ticklabels([])
 
 def run(opts):
     manager = SALPY_scheduler.SAL_scheduler()
@@ -35,14 +35,7 @@ def run(opts):
     plt.ion()
 
     fig, ax1 = plt.subplots(subplot_kw={"projection": PROJECTION})
-    #ax1.grid()
-    #print(type(ax1))
-    ax1.grid(True)
-    #ax1.set_rgrids([0.3333, 0.66666, 1.0], ["60", "30", "0"])
-    #ax1.yaxis.set_ticklabels([])
-
-    #ax1.get_xaxis().set_visible(False)
-    #ax1.grid(True)
+    axisSetup(ax1)
 
     for i, (band_filter, filter_color) in enumerate(FILTER_DICT.items()):
         fig.text(0.1 + i * 0.035, 0.05, band_filter, color=filter_color)
@@ -62,7 +55,6 @@ def run(opts):
                 plt.cla()
 
                 az = np.radians(obs.azimuth)
-                #alt = np.sin(math.radians(obs.dec))
                 alt = 1.0 - math.radians(obs.altitude) / PI_OVER_2
                 color = FILTER_DICT[obs.filter]
                 ellipse = patches.Ellipse((az, alt), LSST_FOV / alt, LSST_FOV, edgecolor='k',
@@ -72,7 +64,7 @@ def run(opts):
 
                 for field in field_list:
                     ax1.add_patch(field)
-                #axisSetup(ax1)
+                axisSetup(ax1)
                 fig_title = "Night {}, MJD {}".format(obs.night, obs.observation_start_mjd)
                 plt.text(-0.3, 1.0, fig_title, transform=ax1.transAxes)
                 plt.draw()
