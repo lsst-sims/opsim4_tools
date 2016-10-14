e6821bae7f356aaade77b772b16c112ab5a54443
Michael Reuter <mareuter@lsst.org>
2016-10-14 08:49:15 -0700

    Updating viewer to latest features.

--- live_view/opsim_live.py
+++ live_view/opsim_live.py
@@ -12,15 +12,19 @@ NUM_FIELDS = 10
 
 LSST_FOV_RADIUS = math.radians(1.75)
 LSST_FOV = LSST_FOV_RADIUS * 2.0
+MOON_SCALE = 6.0
+MOON_DIA = math.radians(0.5 * MOON_SCALE)
 
 ALPHA = 0.4
 FILTER_DICT = collections.OrderedDict([('u', [0, 0, 1, 1]), ('g', [0, 1, 1, 1]),
                                        ('r', [0, 1, 0, 1]), ('i', [1, .5, 0, 1]),
                                        ('z', [1, 0, 0, 1]), ('y', [1, 0, 1, 1])])
+MOON_ALPHA = 0.15
+ASTRO_TWILIGHT = -18.0
 
 def axisSetup(ax):
     ax.grid(True)
-    #ax.xaxis.set_ticklabels([])
+    ax.xaxis.set_ticklabels([])
 
 def run(opts):
     manager = SALPY_scheduler.SAL_scheduler()
@@ -33,13 +37,7 @@ def run(opts):
     plt.ion()
 
     fig, ax1 = plt.subplots(subplot_kw={"projection": PROJECTION})
-    ax1.grid()
-
-    #ax1.grid(True)
-    #ax1.xaxis.set_ticklabels([])
-
-    ax1.get_xaxis().set_visible(False)
-    ax1.grid(True)
+    axisSetup(ax1)
 
     for i, (band_filter, filter_color) in enumerate(FILTER_DICT.items()):
         fig.text(0.41 + i * 0.035, 0.15, band_filter, color=filter_color)
@@ -71,11 +69,29 @@ def run(opts):
 
                 for field in field_list:
                     ax1.add_patch(field)
+
+                if obs.moon_alt > -0.25 * MOON_SCALE:
+                    moon_ra = np.radians(obs.moon_ra)
+                    moon_ra = -(moon_ra - zenith_ra - np.pi) % (np.pi * 2.) - np.pi
+                    moon_dec = np.radians(obs.moon_dec)
+                    alpha = np.max([obs.moon_phase / 100., MOON_ALPHA])
+                    moon = patches.Ellipse((moon_ra, moon_dec), MOON_DIA / np.cos(moon_dec), MOON_DIA,
+                                           color='k', alpha=alpha)
+                    ax1.add_patch(moon)
+
                 axisSetup(ax1)
                 fig_title = "Night {}, MJD {}".format(obs.night, obs.observation_start_mjd)
                 plt.text(0.5, 1.18, fig_title, horizontalalignment='center', transform=ax1.transAxes)
+                moon_phase_text = "Moon Phase: {:.1f}%".format(obs.moon_phase)
+                plt.text(0.8, 1.0, moon_phase_text, transform=ax1.transAxes)
+                if obs.sun_alt <= ASTRO_TWILIGHT:
+                    tom_text = "Night"
+                else:
+                    tom_text = "Twilight"
+                plt.text(0.8, 0.0, tom_text, transform=ax1.transAxes)
+
                 plt.draw()
-                plt.pause(0.00001)
+                plt.pause(0.0001)
 
                 field_list[-1].set_alpha(ALPHA)
                 field_list[-1].set_edgecolor('none')
