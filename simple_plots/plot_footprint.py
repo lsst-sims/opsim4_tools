fa29a7bdd5cf6cf092ece4f526e9ba6bdeabbfdc
Michael Reuter <mareuternh@gmail.com>
2016-11-08 18:39:31 -0700

    Changing region plotting to use new API calls.

--- simple_plots/plot_footprint.py
+++ simple_plots/plot_footprint.py
@@ -7,29 +7,29 @@ from utilities import plot_fields
 from lsst.sims.survey.fields import FieldsDatabase, FieldSelection
 
 def get_scp(fd, fs):
-    query1 = fs.select_region("fieldDec", -90.0, -62.5)
-    query2 = fs.galactic_region(10.0, 0.0, 90.0)
+    query1 = fs.select_region("Dec", -90.0, -62.5)
+    query2 = fs.galactic_region(10.0, 0.0, 90.0, exclusion=True)
     query = fs.combine_queries(('and',), query1, query2)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("SouthCelestialPole", ra, dec)
 
 def get_gp(fd, fs):
-    query = fs.galactic_region(10.0, 0.0, 90.0, exclusion=False)
+    query = fs.galactic_region(10.0, 0.0, 90.0)
     query = fs.combine_queries((), query)
     print(query)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("GalacticPlane", ra, dec)
 
 def get_wfd(fd, fs):
-    query1 = fs.select_region("fieldDec", -62.5, 2.8)
-    query2 = fs.galactic_region(10.0, 0.0, 90.0)
+    query1 = fs.select_region("Dec", -62.5, 2.8)
+    query2 = fs.galactic_region(10.0, 0.0, 90.0, exclusion=True)
     query = fs.combine_queries(('and', ), query1, query2)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("WideFastDeep", ra, dec)
 
 def get_nes(fd, fs):
-    query1 = fs.select_region("fieldEB", -30.0, 10.0)
-    query2 = fs.select_region("fieldDec", 2.8, 90.0)
+    query1 = fs.select_region("EB", -30.0, 10.0)
+    query2 = fs.select_region("Dec", 2.8, 90.0)
     query = fs.combine_queries(('and',), query1, query2)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("NorthEclipticSpur", ra, dec)
@@ -42,30 +42,30 @@ def get_dd(fd, fs):
 
 def get_eb15(fd, fs):
     # Full band
-    query1 = fs.select_region("fieldEB", -15.0, 15.0)
+    query1 = fs.select_region("EB", -15.0, 15.0)
     # Adding NES bump
-    query2 = fs.select_region("fieldEB", -30.0, -15.0)
-    query3 = fs.select_region("fieldDec", 2.8, 90.0)
+    query2 = fs.select_region("EB", -30.0, -15.0)
+    query3 = fs.select_region("Dec", 2.8, 90.0)
     query = fs.combine_queries(('or', 'and'), query1, query2, query3)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("EclipticBand-15", ra, dec)
 
 def get_eb12(fd, fs):
     # Full band
-    query1 = fs.select_region("fieldEB", -12.0, 12.0)
+    query1 = fs.select_region("EB", -12.0, 12.0)
     # Adding NES bump
-    query2 = fs.select_region("fieldEB", -30.0, -12.0)
-    query3 = fs.select_region("fieldDec", 2.5, 90.0)
+    query2 = fs.select_region("EB", -30.0, -12.0)
+    query3 = fs.select_region("Dec", 2.5, 90.0)
     query = fs.combine_queries(('or', 'and'), query1, query2, query3)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("EclipticBand-12", ra, dec)
 
 def get_eb10(fd, fs):
     # Full band
-    query1 = fs.select_region("fieldEB", -10.0, 10.0)
+    query1 = fs.select_region("EB", -10.0, 10.0)
     # Adding NES bump
-    query2 = fs.select_region("fieldEB", -30.0, -10.0)
-    query3 = fs.select_region("fieldDec", 2.0, 90.0)
+    query2 = fs.select_region("EB", -30.0, -10.0)
+    query3 = fs.select_region("Dec", 2.0, 90.0)
     query = fs.combine_queries(('or', 'and'), query1, query2, query3)
     ra, dec = fd.get_ra_dec_arrays(query)
     return ("EclipticBand-10", ra, dec)
